949b9c355b232d55fd08ba55485db6c49b6b605a
Kikiwealth <kikiwealthe@gmail.com>
2025-07-26 15:19:53 -0700

    complete code

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import FeaturedSelection from '../components/FeaturedSelection'
+import Banner from '../components/Banner'
+import Testimonial from '../components/Testimonial'
+import Newsletter from '../components/Newsletter'
+
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      <FeaturedSelection />
+      <Banner />
+      <Testimonial />
+      <Newsletter />
+    </>
+  )
+}
+
+export default Home
