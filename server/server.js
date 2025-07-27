6f53756feb73dafcfaded6d9108ffdf6f65d277a
Kikiwealth <kikiwealthe@gmail.com>
2025-07-26 17:20:55 -0700

    all changes confirmed

--- server/server.js
+++ server/server.js
@@ -21,5 +21,5 @@ app.use('/api/user', userRouter)
 app.use('/api/owner', ownerRouter)
 app.use('/api/bookings', bookingRouter)
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
